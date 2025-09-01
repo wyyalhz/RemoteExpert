74be78329c5e61e32843e0a2929f43b05dab9dc4
Timothy <chenmfwyyx@163.com>
2025-09-01 18:01:03 +0800

    客户端 v0.1.2 	- 修正可能由sendErrorResponse带来的问题的修改对应的注释。

--- server/src/network/protocol/protocol_handlers/user_handler.cpp
+++ server/src/network/protocol/protocol_handlers/user_handler.cpp
@@ -86,7 +86,7 @@ void UserHandler::handleLogin(QTcpSocket* socket, const QJsonObject& data)
         NetworkLogger::authenticationSuccess(clientInfo, username);
     } else {
         sendErrorResponse(socket, 401, "Invalid username or password");
-        // [FIX]为了让客户端统一通过 MSG_LOGIN(不再用 MSG_ERROR)处理登录失败
+        // [FIX]为了让客户端统一通过 MSG_LOGIN(不再用sendErrorResponse)处理登录失败
         QJsonObject fail = MessageBuilder::buildErrorResponse(401, "Invalid username or password");
         sendResponse(socket, MSG_LOGIN, fail);
         
